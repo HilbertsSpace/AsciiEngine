7727b6eaee30934c0485e1547f43aa322f3ba5e0
Alex Sheehan <a.j.sheehan.18@gmail.com>
2020-09-06 16:18:19 -0400

    more setup and stuff

--- ascii_generator.py
+++ ascii_generator.py
@@ -1,11 +1,16 @@
 from utils.exceptions import ValidationError
 
 class AsciiObject(object):
-    def __init__(self, raw_user_input):
-        self.validate_user_input_is_func(raw_user_input)
-        self.raw_user_input = raw_user_input
+    def __init__(self, func, rotation_axis, x_bound, y_bound, step_size):
+        self.validate_user_func(func)
+        self.raw_user_func = func
 
-    def validate_user_input_is_func(self, raw_user_input):
+        self.rotation_axis = rotation_axis
+        self.x_bound = x_bound
+        self.y_bound = y_bound
+        self.step_size = step_size
+
+    def validate_user_func(self, raw_user_input):
         """todo"""
         if raw_user_input[:5] != "func(" or raw_user_input[-1] != ")":
             raise ValidationError("User input needs to be a func")
